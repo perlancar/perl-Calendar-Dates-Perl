6a5f68084648448def95011d725aff699d771904
perlancar (on pc-office) <perlancar@gmail.com>
2019-06-19 17:20:03 +0700

    Initial commit

---
+++ lib/Calendar/Dates/Perl.pm
@@ -0,0 +1,37 @@
+package Calendar::Dates::Perl;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Role::Tiny::With;
+
+with 'Calendar::DatesRoles::DataPreparer::CalendarVar::FromDATA::Simple';
+with 'Calendar::DatesRoles::DataUser::CalendarVar';
+
+1;
+# ABSTRACT: Dates/anniversaries related to Perl
+
+=head1 DESCRIPTION
+
+
+=head1 prepend:SEE ALSO
+
+L<perlhist>
+
+=cut
+
+__DATA__
+
+# people
+1954-09-27;Larry Wall's birthday;anniversary
+
+# releases
+1987-12-18;Perl 1.0 released;anniversary
+1991-03-21;Perl 4.0 released;anniversary,low-priority
+1994-10-17;Perl 5.0 released;anniversary
+
+# 2019
